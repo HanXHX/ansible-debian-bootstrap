0e344ed9d598abf0a8304566c857838ac2d36534
Emilien Mantel <emilien.mantel@gmail.com>
2015-10-16 14:31:44 +0200

    Debug idempotence on travis

--- tests/test-travis.sh
+++ tests/test-travis.sh
@@ -15,7 +15,18 @@ ansible-playbook -i $INVENTORY_FILE -c local --syntax-check -vv $PLAYBOOK
 ansible-playbook -i $INVENTORY_FILE -c local --sudo -vv $PLAYBOOK
 
 # Check indempotence
+idpt=fail
 ansible-playbook -i $INVENTORY_FILE -c local --sudo -vv $PLAYBOOK \
+| tee replay.txt \
 | grep -q 'changed=0.*failed=0' \
-&& (echo 'Idempotence test: pass' && exit 0) \
-|| (echo 'Idempotence test: fail' && exit 1)
+&& idpt=ok 
+
+if [ "$idpt" == "ok" ]
+then
+	echo 'Idempotence test: pass'
+	exit 0
+else
+	cat replay.txt
+	echo 'Idempotence test: fail'
+	exit 1
+fi
